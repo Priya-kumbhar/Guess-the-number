f5b9f653bffaae8be75763540040912da7eccc55
Prashant <priyankakumbhar.2405@gmail>
2025-08-20 21:23:19 +0530

    small flaws

---
+++ project/script.js
@@ -0,0 +1,13 @@
+let books=[ "Algorithms", "Clean Code", "Data Structures",
+    "Design Patterns", "Java Basics", "JavaScript",
+    "Operating System", "Python Programming",
+    "Theory of Computation", "Web Development"
+ ];
+function binarySearch(arr,target){
+    let left=0;
+    right=arr.length-1;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if()
+    }
+}
